022a853266534a97813f55c32d86fdfbef33f77b
kjhonggg95 <kjhonggg95@gmail.com>
2022-09-25 01:09:36 +0900

    2110번: 공유기 설치

--- Gold/2110.cpp
+++ Gold/2110.cpp
@@ -3,40 +3,48 @@
 using namespace std;
 using ll = long long;
 
+vector<ll> v;
+int n, c;
+
+bool check(int mid)
+{
+    ll cnt = 1;   // 첫 번째 집 공유기 카운트
+    ll cur = v[0]; // 첫 번째 집 기준
+
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] - cur >= mid)
+        {
+            cnt++;
+            cur = v[i];
+        }
+    }
+    return cnt >= c;
+}
+
 int main()
 {
     fastio;
-    int n,c;
-    cin >> n >> c; // 5 3
-    vector<ll> v(n);
-    for(int i = 0; i<n;i++)
-        cin >> v[i]; // 1 2 8 4 9
-    
+    cin >> n >> c;
+    v.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
     sort(v.begin(), v.end()); // 1 2 4 8 9
 
-    ll st = 1;             // 시작 거리 // 1 // 1 // 2 // 3
-    ll en = v[n-1] - v[0]; // 끝 거리   // 8 // 3 // 3 // 3
-
-    while(st <= en)
+    // 간격 범위 경계값 포함
+    ll lo = 0;                   
+    ll hi = v[n - 1] - v[0] + 1;
+    
+    while (lo + 1 < hi)
     {
-        ll cnt = 1;
-        ll mid = (st + en ) / 2; // 간격
-        ll cur = v[0];
+        ll mid = (lo + hi) / 2; // 간격
 
-        for(int i = 1;i< n;i++)
-        {
-            if(v[i] - cur >= mid)
-            {
-                cnt++;
-                cur = v[i];
-            }
-        }
-
-        if(cnt >= c) // 공유기 수 줄이기 == 간격 넓히기
-            st = mid + 1;
-        else // 공유기 수 늘리기 == 간격 좁히기
-            en = mid - 1; // 3
+        if (!check(mid))
+            hi = mid; // 공유기 적을 때 -> 간격 줄이기
+        else
+            lo = mid; // 공유기 많을 때 -> 간격 늘리기
     }
 
-    cout << (st + en) / 2;
+    cout << lo;
 }

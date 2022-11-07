3aa2990d3af9c3ca43c5486b62c636d204d5f023
kjhonggg95 <kjhonggg95@gmail.com>
2022-11-07 18:58:17 +0900

    13537번: 수열과 쿼리 1

---
+++ Platinum/13537.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+
+int n, sz = 1;
+vector<vector<int>> tree;
+
+void build()
+{
+    for(int i = sz - 1; i>0;i--) // 7 6 5 4 3 2 1
+    {
+        tree[i].resize(tree[i*2].size() + tree[i*2 + 1].size());
+        merge(all(tree[i*2]), all(tree[i*2 +1]), tree[i].begin());
+    }
+}
+
+int query(int node, int nodeL, int nodeR, int L, int R, int k)
+{   
+    if(R < nodeL || nodeR < L) return 0;
+    if(L <= nodeL &&  nodeR <= R) return tree[node].end() -  upper_bound(all(tree[node]), k);   
+    return query(node*2, nodeL, (nodeL + nodeR) / 2, L, R, k) + query(node*2 + 1, (nodeL+nodeR) / 2 + 1, nodeR, L, R, k);
+}
+
+int main()
+{
+    fastio;
+    cin >> n;
+    while(sz < n) sz <<= 1;
+    tree.resize(sz * 2);
+    for(int i = 0;i<n;i++)
+    {
+        int x; cin >> x;
+        tree[sz+i].push_back(x);
+    }
+
+    build();
+
+   int m;
+   cin >> m;
+   while(m--)
+   {
+        int a, b, c; 
+        cin >> a >> b >> c;
+        cout << query(1, 1, sz, a, b, c) << '\n';
+   }
+}

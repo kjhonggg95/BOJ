89a720a2e28bceb5b05ba333c185ad8867a7c811
kjhonggg95 <kjhonggg95@gmail.com>
2022-11-09 03:47:51 +0900

    12014번: 주식

---
+++ Gold/12014.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    fastio;
+    int t;
+    cin >> t;
+    for(int tc = 1;tc<=t;tc++)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        vector<int> lis;
+        for(int i = 0;i<n;i++)
+            cin >> arr[i];
+        lis.push_back(arr[0]);
+        for(int i = 1;i<n;i++)
+            if(lis.back() < arr[i]) lis.push_back(arr[i]);
+            else *lower_bound(all(lis), arr[i]) = arr[i];
+        
+        cout << "Case #" << tc << '\n';
+        if(lis.size() >= k) cout << "1\n";
+        else cout << "0\n";
+    }
+}

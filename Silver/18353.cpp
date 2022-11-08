ea358cbe656d7ae0dc28e4bbba739052950bf83e
kjhonggg95 <kjhonggg95@gmail.com>
2022-11-09 03:49:47 +0900

    18353번: 병사 배치하기

---
+++ Silver/18353.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    fastio;
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    vector<int> lis;
+    for(int i = 0;i<n;i++)
+        cin >> arr[i];
+    reverse(all(arr));
+    
+    lis.push_back(arr[0]);
+    for(int i =1;i<n;i++)
+         if(lis.back() < arr[i]) lis.push_back(arr[i]);
+         else *lower_bound(all(lis), arr[i]) = arr[i];
+    
+    cout << n - lis.size();
+}
